58e70c0a1efd1e07bd7ac6f92cf7015ca07ffcb4
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-12-09 19:44:15 +0900

    優先サーバー

--- misskey-emoji.ts
+++ misskey-emoji.ts
@@ -17,18 +17,25 @@ export const command = new SlashCommandBuilder()
 			.setDescription("emojis name")
 			.setDescriptionLocalization("ja", "取得する絵文字の名前")
 			.setRequired(true)
-	);
-/*.addStringOption((o) =>
+	)
+	.addStringOption((o) =>
 		o
 			.setName(Options.server_name)
 			.setNameLocalization("ja", "サーバー名")
-			.setDescription("Server name to get emoji Default: misskey.io")
-			.setDescriptionLocalization("ja", "絵文字を持ってくるサーバー名 既定: misskey.io")
+			.setDescription("Server to prioritize only this time")
+			.setDescriptionLocalization("ja", "今回だけ優先するサーバー")
 			.setRequired(false)
-	);*/
+	);
 export const execute: CommandHandler = (interaction, user) => {
 	const emoji_name = interaction.options
 		.getString(Options.emoji_name, true)
 		.replace(/<?:([a-zA-Z0-9_-~]*):(?:\d+>)?/, "$1");
-	interaction.reply(user.emojiResolver.get(emoji_name)?.url || "エラー");
+	const server_name = interaction.options
+		.getString(Options.server_name)
+		?.replace(/(https?:)?\/\/([a-zA-Z0-9-.]+\.[a-zA-Z0-9-]+).*/, "$1");
+	if (server_name) {
+		interaction.reply(user.emojiResolver.get(emoji_name, [server_name])?.url || "エラー");
+	} else {
+		interaction.reply(user.emojiResolver.get(emoji_name)?.url || "エラー");
+	}
 };
